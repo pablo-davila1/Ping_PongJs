6d0c03fb8931cc3d49a57973a27bc7265c74a8c6
pablo-davila1 <62685470+pablo-davila1@users.noreply.github.com>
2022-04-21 21:55:48 -0500

    Scorlls
    En este commit se crean las funciones draw() y Bar(), con el fin de agragar a la escena las barras scrolls.

--- PongJS/pong/main.js
+++ PongJS/pong/main.js
@@ -6,12 +6,34 @@
         this.game_over = false;
         this.bars=[];
         this.ball = null;
-    }
+    };
     self.Board.prototype ={
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
+        }
+    };
+
+})();
+
+(function(){
+    self.Bar = function(x,y,width,height,board){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.board=board;
+        this.board.bars.push(this);
+        this.kind ="rectangle";
+    }
+
+    self.Bar.prototype={ // quiiiiiii <-------- 
+        down:function(){
+
+        },
+        up:function(){
+
         }
     }
 
@@ -26,12 +48,37 @@
         this.ctx = canvas.getContext("2d");
         
     }
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for(var i = this.board.elements.length -1 ; i>=0; i--){
+                var el = this.board.elements[i];
+                draw(this.ctx,el);
+            };
+        }
+    }   
+
+    function draw(ctx,element){                                     //<----------
+        if(element != null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y, element.width, element.height);
+                    break;
+                    
+            }
+        }
+
+    }
 })();
+
+
 
 window.addEventListener("load",main);
 
 function main(){
     var board = new Board(800,400);
+    var bar = new Bar(20,100,40,100,board);
     var canvas = document.getElementById("canvas");
     var board_view = new BoardView(canvas,board);
+    board_view.draw();
 }
